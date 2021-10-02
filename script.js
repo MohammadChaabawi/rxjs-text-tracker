95d2d2bd77f02f398e7b4b63d8125df9f0f77a82
MohammadChaabawi <codeclubhub@gmail.com>
2021-10-02 18:08:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var { fromEvent } = rxjs;
+var { map } = rxjs.operators;
+
+var countElement = document.getElementById("toCount");
+var resultElement = document.getElementById("result");
+
+var source = fromEvent(countElement, "keyup").pipe(
+  map(function (event) {
+    var input = event.target.value;
+    var characters = input.length;
+    var words = input.split(" ").filter(function (word) {
+      return word !== "";
+    }).length;
+    return (
+      "length of input is: " +
+      characters +
+      " and the amount of words is: " +
+      words
+    );
+  })
+);
+
+source.subscribe(function (text) {
+    resultElement.innerHTML = text
+});
